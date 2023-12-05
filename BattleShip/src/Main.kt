dc5816868f5f665d34b481b5d950cb0ae307d7e6
Maggie <maggie@MBP-de-orcas.lan>
2023-12-05 23:00:47 +0000

    5DEC2023

--- BattleShip/src/Main.kt
+++ BattleShip/src/Main.kt
@@ -1,3 +1,6 @@
+import java.io.File
+import java.io.PrintWriter
+
 var numLinhas = -1
 var numColunas = -1
 var tabuleiroHumano: Array<Array<Char?>> = emptyArray()
@@ -11,8 +14,14 @@ fun calculaNumNavios(numLinhas: Int, numColunas: Int): Array<Int>{
     return emptyArray()
 }
 
+/**
+ * Cria um tabuleiro vazio com o tamanho definido.
+ * @param numLinhas Numero inteiro com o numero de linhas do tabuleiro
+ * @param numColunas Numero inteiro com o numero de colunas do tabuleiro
+ * @return Array de arrays de Char? com todos os char colocados a null
+ */
 fun criaTabuleiroVazio(numLinhas: Int, numColunas: Int): Array<Array<Char?>>{
-    return emptyArray()
+    return Array(numLinhas){Array(numColunas){null}}
 }
 
 fun coordenadaContida(tabuleiro: Array<Array<Char?>>, linha: Int, coluna: Int): Boolean{
@@ -37,8 +46,19 @@ fun gerarCoordenadasFronteira(tabuleiro: Array<Array<Char?>>, linha: Int, coluna
     return emptyArray()
 }
 
+/**
+ * Verifica se todos os pares do array coordenadas são coordenadas válidas do tabuleiro enviado.
+ * @param tabuleiro Array de arrays de Char? com o tabuleiro a verificar
+ * @param coordenadas Array de pares de inteiros com as coordenadas a verificar
+ * @return True, se todos os pares de coordenadas forem válidos, ou false, se algum não for
+ */
 fun estaLivre(tabuleiro: Array<Array<Char?>>, coordenadas: Array<Pair<Int, Int>>): Boolean {
-    return false
+    for (coordenada in coordenadas){
+        if (tabuleiro[coordenada.first][coordenada.second] != null){
+            return false
+        }
+    }
+    return true
 }
 
 fun insereNavioSimples(tabuleiro: Array<Array<Char?>>, linha: Int, coluna: Int, dimensao: Int): Boolean{
@@ -60,8 +80,40 @@ fun navioCompleto(tabuleiro: Array<Array<Char?>>, linha: Int, coluna: Int): Bool
     return false
 }
 
-fun obtemMapa(tabuleiro: Array<Array<Char?>>, tabuleiroHumano: Boolean): Array<String>{
-    return emptyArray()
+/**
+ * Cria o desenho do mapa correspondente ao tabuleiro que recebe, para poder mostrar ao utilizador.
+ * @param tabuleiro Array<Array<Char?>> do tabuleiro que queremos transformar (tabuleiroHumano OU tabuleiroComputador)
+ * @param tabuleiroReal True, se o tabuleiro for o real, ou false, se for um tabuleiro de palpites
+ * @return Array de strings contendo o mapa para mostrar no stdout (incluindo a legenda)
+ */
+fun obtemMapa(tabuleiro: Array<Array<Char?>>, tabuleiroReal: Boolean): Array<String>{
+    val mapa = Array(tabuleiro.size + 1){""}
+    mapa[0] = "| " + criaLegendaHorizontal(tabuleiro[0].size) + " |"
+        for (linha in 0 until tabuleiro.size){
+            mapa[linha + 1] += "|"
+            for (coluna in 0 until tabuleiro[linha].size){
+                if (tabuleiroReal){
+                    when (tabuleiro[linha][coluna]){
+                        null -> mapa[linha + 1] += " ~ |"
+                        '1' -> mapa[linha + 1] += " 1 |"
+                        '2' -> mapa[linha + 1] += " 2 |"
+                        '3' -> mapa[linha + 1] += " 3 |"
+                        '4' -> mapa[linha + 1] += " 4 |"
+                    }
+                } else {
+                    mapa[linha + 1] += when (tabuleiro[linha][coluna]){
+                       '1' -> if (navioCompleto(tabuleiro, linha, coluna)) " 1 |" else " \u2081 |"
+                       '2' -> if (navioCompleto(tabuleiro, linha, coluna)) " 2 |" else " \u2082 |"
+                       '3' -> if (navioCompleto(tabuleiro, linha, coluna)) " 3 |" else " \u2083 |"
+                       '4' -> if (navioCompleto(tabuleiro, linha, coluna)) " 4 |" else " \u2084 |"
+                       'X' -> " X |"
+                       else -> " ? |"
+                   }
+                }
+            }
+            mapa[linha + 1] += " $linha"
+        }
+    return mapa
 }
 
 fun lancaTiro(tabuleiroReal: Array<Array<Char?>>, tabuleiroPalpites: Array<Array<Char?>>, coordenadas: Pair<Int,Int>): String{
@@ -86,9 +138,84 @@ fun lerJogo(ficheiro: String, tipoTabuleiro: Int):Array<Char?>{
     return emptyArray()
 }
 
+/**
+ * Recebe um PrintWriter aberto do ficheiro onde gravamos os tabuleiros de jogo e grava um determinado
+ * tabuleiro lá para dentro. Coloca um newLine, depois coloca o tabuleiro e termina no fim do tabuleiro, sem newLine no
+ * final.
+ * @param jogoGravado PrintWriter para o ficheiro onde queremos gravar o tabuleiro (já no local onde o queremos gravar)
+ * @param tabuleiro Tabuleiro que pretendemos colocar no ficheiro
+ */
+fun gravarTabuleiro(jogoGravado: PrintWriter, tabuleiro: Array<Array<Char?>>){
+    for (linha in 0 until tabuleiro.size){
+        jogoGravado.println()
+        for (coluna in 0 until tabuleiro[0].size){
+            when(tabuleiro[linha][coluna]){
+                'X' -> jogoGravado.print("X")
+                '2','3','4' -> jogoGravado.print(tabuleiro[linha][coluna].toString().toInt())
+            }
+            if (coluna < tabuleiro[0].size - 1){
+                jogoGravado.print(",")
+            }
+        }
+    }
+}
+
+/**
+ * Grava o jogo no formato:
+ * 5x5
+ *
+ * Jogador
+ * Real
+ * ,,,,3
+ * 2,,,2,
+ * 3,,3,,
+ * ,4,4,,4
+ * 3,,3,,
+ *
+ * Jogador
+ * Palpites
+ * ,,,,3
+ * 2,,,2,
+ * 3,,3,,
+ * ,4,4,,4
+ * 3,,3,,
+ *
+ * Computador
+ * Real
+ * ,,,,3
+ * 2,,,2,
+ * 3,,3,,
+ * ,4,4,,4
+ * 3,,3,,
+ *
+ * Computador
+ * Palpites
+ * ,,,,3
+ * 2,,,2,
+ * 3,,3,,
+ * ,4,4,,4
+ * 3,,3,,
+ *
+ * @param ficheiro String com o nome do ficheiro onde gravar
+ * @param tabuleiroHumano Array<Array<Char?>> com o tabuleiro real do jogador
+ * @param tabuleiroPalpitesDoHumano Array<Array<Char?>> com o tabuleiro dos palpites jogador
+ * @param tabuleiroComputador Array<Array<Char?>> com o tabuleiro real do computador
+ * @param tabuleiroPalpitesDoComputador Array<Array<Char?>> com o tabuleiro dos palpites do computador
+ *
+ */
 fun gravarJogo(ficheiro: String, tabuleiroHumano: Array<Array<Char?>>, tabuleiroPalpitesDoHumano: Array<Array<Char?>>,
                tabuleiroComputador: Array<Array<Char?>>, tabuleiroPalpitesDoComputador: Array<Array<Char?>>){
-
+    val jogoGravado = File(ficheiro).printWriter()
+    jogoGravado.print("${tabuleiroHumano.size}x${tabuleiroHumano[0].size}")
+    jogoGravado.print("\n\nJogador\nReal")
+    gravarTabuleiro(jogoGravado, tabuleiroHumano)
+    jogoGravado.print("\n\nJogador\nPalpites")
+    gravarTabuleiro(jogoGravado, tabuleiroPalpitesDoHumano)
+    jogoGravado.print("\n\nComputador\nReal")
+    gravarTabuleiro(jogoGravado, tabuleiroComputador)
+    jogoGravado.print("\n\nComputador\nPalpites")
+    gravarTabuleiro(jogoGravado, tabuleiroPalpitesDoComputador)
+    jogoGravado.close()
 }
 
 // Funcoes parte 1
@@ -317,7 +444,7 @@ fun tabuleiro(): Array<String>?{
 }
 
 fun main() {
-    while (true) {
+/*    while (true) {
         printMenuJogo()
         val opcao = escolhaOpcao()
         when (opcao) {
@@ -325,6 +452,15 @@ fun main() {
             2, 3, 4 -> println("!!! POR IMPLEMENTAR, tente novamente")
             1 -> tabuleiro()
         }
-    }
+    }*/
+    val mapa = arrayOf(arrayOf(null,null,'1',null,'3'), arrayOf('2',null,null,'2',null), arrayOf('3',null,'3',null,'1'), arrayOf(null,'4','4',null,'4'), arrayOf('3',null,'3',null,'1'))
+    val mapaDesenhado = obtemMapa(mapa, false)
+    println(mapaDesenhado[0])
+    println(mapaDesenhado[1])
+    println(mapaDesenhado[2])
+    println(mapaDesenhado[3])
+    println(mapaDesenhado[4])
+    gravarJogo("ficheiro.txt", mapa, mapa, mapa, mapa)
+
 }
 
